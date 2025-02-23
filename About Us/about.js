c78e11a4398b7432e9becac9e4c5242802933427
Malthessers <matthew.dumaguits@gmail.com>
2025-02-23 22:02:33 +0800

    Add files via upload

---
+++ About Us/about.js
@@ -0,0 +1,18 @@
+let menu = document.querySelector("#menu-icon"); // Use # for ID
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x"); // Fix classList usage
+    navlist.classList.toggle("open");
+};
+
+window.onscroll = () => {
+    menu.classList.remove("bx-x"); // Fix classList usage
+    navlist.classList.remove("open");
+};
+
+const sr = ScrollReveal ({
+        distance: '30px',
+        duration: 2600,
+        reset: true
+})
